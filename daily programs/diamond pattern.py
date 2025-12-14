a59f0db48f686bdc5ba1c344f44bf1fcc498e8ff
saikukatla <kvnsaiteja2002@gmail.com>
2025-12-14 18:29:02 +0530

    14 12 2025

--- daily programs/diamond pattern.py
+++ daily programs/diamond pattern.py
@@ -1,19 +1,13 @@
-n = int(input("Enter the number of rows (odd number for symmetry): "))
-# Upper half of the diamond
+n = int(input("Enter the number of rows: "))
 for i in range(1, n + 1):
-    # Print leading spaces
     for j in range(n - i):
         print(" ", end="")
-    # Print stars
     for j in range(2 * i - 1):
         print("*", end="")
     print()
-# Lower half of the diamond
 for i in range(n - 1, 0, -1):
-    # Print leading spaces
     for j in range(n - i):
         print(" ", end="")
-    # Print stars
     for j in range(2 * i - 1):
         print("*", end="")
     print()
